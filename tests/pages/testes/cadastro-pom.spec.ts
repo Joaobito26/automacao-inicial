b894495fcc56ea8412626e17ec2a02b22af3aaba
Joaobito26 <joao.silva@amigotech.com.br>
2026-04-20 16:55:05 -0300

    Refactor signup test and page object methods for clarity and consistency

--- tests/pages/testes/cadastro-pom.spec.ts
+++ tests/pages/testes/cadastro-pom.spec.ts
@@ -1,16 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { SignupLoginPage } from './seletores.page';
 
-test('Deve iniciar o processo de cadastro com sucesso via POM', async ({ page }) => {
+test('Deve iniciar e finalizar com sucesso o cadastro de usuário', async ({ page }) => {
   const signupPage = new SignupLoginPage(page);
   
   // Dados dinâmicos para evitar erro de e-mail já cadastrado
-  const emailRandom = `tester_${Date.now()}@test.com`;
-
+  // const emailRandom = `tester_${Date.now()}@test.com`;
+  
+  // Mockado
+  const emailRandom = `tester_123456@test.com`;
+  
   await signupPage.acessarPagina();
   await signupPage.irParaLoginSignup();
-  await signupPage.realizarNovoRegistro('João Silva', emailRandom);
+  await signupPage.criarNovoCadastro('João Silva', emailRandom);
 
   // Asserção: Verifica se chegamos na página de detalhes da conta
   await expect(page.locator('b', { hasText: 'Enter Account Information' })).toBeVisible();
+
+  await signupPage.preencherFormularioCadastro({ nome: 'João Silva', email: emailRandom });
 });
--- tests/pages/testes/seletores.page.ts
+++ tests/pages/testes/seletores.page.ts
@@ -6,15 +6,19 @@ export class SignupLoginPage {
   readonly signupEmailInput: Locator;
   readonly signupButton: Locator;
   readonly loginLink: Locator;
+  readonly titleButton: Locator;
+  readonly passwordInput: Locator;
+
 
   constructor(page: Page) {
     this.page = page;
-
 
     this.loginLink = page.getByRole('link', { name: ' Signup / Login' });
     this.signupNameInput = page.getByPlaceholder('Name');
     this.signupEmailInput = page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address');
     this.signupButton = page.getByRole('button', { name: 'Signup' });
+    this.titleButton = page.locator('#id_gender1');
+    this.passwordInput = page.locator('[data-qa="password"]');
   }
 
   async acessarPagina() {
@@ -25,9 +29,17 @@ export class SignupLoginPage {
     await this.loginLink.click();
   }
 
-  async NovoCadastro(nome: string, email: string) {
+  async criarNovoCadastro(nome: string, email: string) {
     await this.signupNameInput.fill(nome);
     await this.signupEmailInput.fill(email);
     await this.signupButton.click();
+  }
+  
+  async preencherFormularioCadastro(dados: { nome: string; email: string }) {
+    
+    await this.titleButton.check();
+    // await this.signupNameInput.fill(dados.nome);
+    // await this.signupEmailInput.fill(dados.email);
+    await this.passwordInput.fill('senhajon123');
   }
 }
